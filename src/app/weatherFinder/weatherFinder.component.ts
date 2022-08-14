7932fa85bd41e8e473a8f47b212ff455598a458d
Git Server Admin <admin@git-server>
2022-08-14 17:05:14 +0000

    Add initial repository

---
+++ src/app/weatherFinder/weatherFinder.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+interface CityWeather {
+  name: string;
+  weather: string;
+  status: string[];
+}
+
+interface ApiResponse {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: CityWeather[];
+}
+
+@Component({
+  selector: 'weather-finder',
+  templateUrl: './weatherFinder.component.html',
+  styleUrls: ['./weatherFinder.component.scss']
+})
+export class WeatherFinder implements OnInit {
+
+  ngOnInit(): void {
+  }
+}
