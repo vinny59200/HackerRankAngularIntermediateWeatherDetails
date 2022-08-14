55557a4e4cdfcdee9c7daf55ea03fb7d44088bf7
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2022-08-14 19:35:11 +0200

    init

--- src/app/weatherFinder/weatherFinder.component.ts
+++ src/app/weatherFinder/weatherFinder.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 interface CityWeather {
   name: string;
   weather: string;
@@ -14,6 +16,13 @@ interface ApiResponse {
   data: CityWeather[];
 }
 
+interface data {
+  name: string;
+  temperature: string;
+  wind: string;
+  humidity: string;
+}
+
 @Component({
   selector: 'weather-finder',
   templateUrl: './weatherFinder.component.html',
@@ -21,6 +30,36 @@ interface ApiResponse {
 })
 export class WeatherFinder implements OnInit {
 
+  exist: boolean;
+  town: string = '';
+  temperature: string = '';
+  wind: string = '';
+  humidity: string = '';
+  isSunny: boolean;
+
   ngOnInit(): void {
+  }
+
+
+  private apiurl = "https://jsonmock.hackerrank.com/api/weather?name=";
+  constructor(private http: HttpClient) { }
+
+
+  onTownChange(): void {
+    console.log(this.town);
+    // let string = this.celsius;
+    // let num = parseInt(string) * 9 / 5 + 32;
+    // this.fahrenheit = num.toString();
+
+    this.http.get(this.apiurl + this.town).subscribe((filteredResult: any) => {
+      console.log(filteredResult);
+      if (filteredResult) this.exist = true; else this.exist = false;
+      if (this.exist) {
+        this.temperature = filteredResult.data[0].weather;
+        this.isSunny = +(this.temperature.split(" ")[0])>=20;
+        this.humidity = filteredResult.data[0].status[1];
+        this.wind = filteredResult.data[0].status[0];
+      }
+    });
   }
 }
